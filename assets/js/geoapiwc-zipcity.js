7d7c9972ca3a288f446a266eca7f702f74325588
denis.music <deniss9oo@gmail.com>
2024-03-17 15:47:37 +0100

    Refactored so that passed script object from WP is used in JS - passed plugin settings values

--- assets/js/geoapiwc-zipcity.js
+++ assets/js/geoapiwc-zipcity.js
@@ -1,14 +1,18 @@
 import {ZipToCity} from "./GeoApiWC/ZipToCity.js";
 
 ((that) => {
-    const document = that.document;
+    const
+        document = that.document,
+        geoapiwc = that.geoapiwc
+    ;
+    console.log (geoapiwc);
 
     that.window.addEventListener('load', () => {
         const zipCity = new ZipToCity(
             document,
-            '#billing_country', // this needs to be pushed as translatable object data
-            '#billing_postcode', // this needs to be pushed as translatable object data
-            '#billing_city' // this needs to be pushed as translatable object data
+            geoapiwc.country_field_id,
+            geoapiwc.postcode_field_id,
+            geoapiwc.city_field_id
         );
 
         zipCity.getZipElement().addEventListener('focusout', () => {
