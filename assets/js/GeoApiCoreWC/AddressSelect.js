cc315a8df750e020a94c0ac6db4bf17cc768ef6a
denis.music <deniss9oo@gmail.com>
2024-03-25 00:16:40 +0100

    Created ApiOverlay class and methods to apply overlay html and loader and to remove it, created HTML service class for addresses select type of API, refactored main AddressSelect api fetch method

--- assets/js/GeoApiCoreWC/AddressSelect.js
+++ assets/js/GeoApiCoreWC/AddressSelect.js
@@ -1,4 +1,6 @@
 import {AddressToCity} from "./AddressToCity.js";
+import {ApiOverlay} from "./ApiOverlay.js";
+import {AddressSelectHtmlService} from "./AddressSelectHtmlService.js";
 
 export class AddressSelect extends AddressToCity {
     constructor(document, api_url, country_target, address_target, zip_target, city_target) {
@@ -6,26 +8,37 @@ export class AddressSelect extends AddressToCity {
     }
 
     async apiCallGeoAddressesData() {
-        const
-            countryCode = this.getCountryElement().options[this.getCountryElement().selectedIndex].value,
-            addressValue = this.getAddressElement().value,
-            response = await fetch(this.apiUrl + encodeURIComponent(addressValue) + '/' + countryCode)
-        ;
+        const Overlay = new ApiOverlay();
+        const HtmlService = new AddressSelectHtmlService();
+        let response = null;
+        let apiData  = null;
 
-        if (! response.ok) {
-            console.error("API request failed with status:", response.status);
-            return false;
+        try {
+            const countryCode  = this.getCountryElement().options[this.getCountryElement().selectedIndex].value;
+            const addressValue = this.getAddressElement().value;
+
+            Overlay.show(); // Show overlay and loader
+
+            response = await fetch(this.apiUrl + encodeURIComponent(addressValue) + '/' + countryCode);
+
+            if (!response.ok) {
+                throw new Error(`API request failed with status: ${response.status}`);
+            }
+
+            apiData = await response.json();
+
+            if (!apiData) {
+                throw new Error('Invalid API response');
+            }
+
+            // Add new HTML with addresses as list to DOM
+            // after the address input field
+            HtmlService.renderAddresses(apiData, this.getAddressElement());
+        } catch (error) {
+            console.error('Error fetching addresses:', error);
+        } finally {
+            Overlay.remove(); // Always remove overlay and loader
         }
-
-        const apiData = await response.json();
-
-        if (! apiData) {
-            return false;
-        }
-
-        // Add new HTML with addresses as list to DOM
-        // after the address input field
-        this.#appendSelectAddressHtml(apiData);
 
         // Add click event handlers for single-address elements
         const addressContainer = this.getAddressElement().parentElement.querySelector('.geoapiwc-content--api-data--container');
@@ -33,53 +46,8 @@ export class AddressSelect extends AddressToCity {
             .querySelectorAll('.single-address')
             .forEach((addressElement) => {
                 addressElement.addEventListener('click', (event) => {
-                    this.#selectAddressHandler(event, this.getAddressElement(), this.getZipElement(), this.getCityElement(), addressContainer)
+                    HtmlService.selectAddressHandler(event, this.getAddressElement(), this.getZipElement(), this.getCityElement(), addressContainer)
                 });
             });
-    }
-
-    #createAddressContainer(addressFieldHeight) {
-        const _addressContainer = document.createElement('span');
-
-        _addressContainer.classList.add('geoapiwc-content--api-data--container');
-        _addressContainer.style.top = `${addressFieldHeight}px`;
-
-        return _addressContainer;
-    }
-
-    #createSelectAddressesHtml(apiAddresses) {
-        let html = '';
-        apiAddresses.forEach((apiAddress) => {
-            html += `<span class="single-address" data-address="${apiAddress.address}" data-street="${apiAddress.street}" data-zip="${apiAddress.zip}" data-city="${apiAddress.name}">${apiAddress.address}</span>`;
-        });
-
-        return html;
-    }
-
-    #appendSelectAddressHtml(apiData) {
-        const addressContainer = this.#createAddressContainer(this.getAddressElement().offsetHeight);
-
-        addressContainer.innerHTML = this.#createSelectAddressesHtml(apiData);
-
-        // Add a class to highlight suggested address container
-        this.addressElement.parentElement.classList.add('geoapiwc-parent-wrapper');
-
-        // Append the address container to the parent element
-        this.addressElement.parentElement.appendChild(addressContainer);
-    }
-
-    #selectAddressHandler(event, addressElement, zipElement, cityElement, addressContainer) {
-        const selectedAddressElement = event.target; // Get the clicked address element
-        const address = selectedAddressElement.dataset.address;
-        const zip = selectedAddressElement.dataset.zip;
-        const city = selectedAddressElement.dataset.city;
-
-        //this.addressElement.value = address; // Update address field
-        zipElement.value = zip; // Update ZIP field
-        cityElement.value = city; // Update city field
-
-        // Hide the address suggestions container
-        addressElement.parentElement.classList.remove('geoapiwc-parent-wrapper'); // Assuming this class hides the container
-        addressContainer.remove(); // remove all addresses from DOM
     }
 }
